d4c22651536765adf38622274e512ee60715d4c9
Lewis <lewisbennett669@gmail.com>
2021-04-13 22:27:11 +0100

    Added IP lookup sample project

---
+++ Samples/Sample.IPLookup/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading.Tasks;
+using WeatherAPI;
+
+namespace Sample.IPLookup
+{
+    public static class Program
+    {
+        public const string IPAddress = "";
+
+        public static async Task Main()
+        {
+            // Create a new WeatherAPI API client with API key.
+            var weatherApiClient = new WeatherAPIClient(Environment.GetEnvironmentVariable("API_KEY"));
+
+            var ipLookup = await weatherApiClient.IPLookup.LookIPAddressAsync(IPAddress).ConfigureAwait(false);
+
+            var stringFormat = "The IP address is located within {0}, {1}, {2}.";
+
+            Console.WriteLine(string.Format(stringFormat, ipLookup.City, ipLookup.Country, ipLookup.Continent));
+
+            // Keeps the console window open at the end of the program.
+            Console.ReadLine();
+        }
+    }
+}
