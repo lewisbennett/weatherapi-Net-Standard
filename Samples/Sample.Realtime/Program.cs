bcd14c265e5e4f914f56a75ecd302ef394088810
Lewis <lewisbennett669@gmail.com>
2021-04-13 17:34:10 +0100

    Update Program.cs

--- Samples/Sample.Realtime/Program.cs
+++ Samples/Sample.Realtime/Program.cs
@@ -13,14 +13,16 @@ namespace Sample.Realtime
 
             var currentWeather = await weatherApiClient.Realtime.GetCurrentAsync().ConfigureAwait(false);
 
-            Console.WriteLine($"The weather in {currentWeather.Location.Name}, {currentWeather.Location.Country} is {currentWeather.Current.TemperatureC} degrees C and {currentWeather.Current.Condition.Description}!");
+            var stringFormat = "The weather in {0}, {1} is {2} degrees C and {3}!";
+
+            Console.WriteLine(string.Format(stringFormat, currentWeather.Location.Name, currentWeather.Location.Country, currentWeather.Current.TemperatureC, currentWeather.Current.Condition.Description));
 
             var request = RequestQuery.CreateFromCityName("London")
                 .WithLanguage("fr");
 
-            var londonWeather = await weatherApiClient.Realtime.GetCurrentAsync(request).ConfigureAwait(false);
+            var londonWeather = await weatherApiClient.Realtime.GetCurrentAsync(request, true).ConfigureAwait(false);
 
-            Console.WriteLine($"The weather in {londonWeather.Location.Name}, {londonWeather.Location.Country} is {londonWeather.Current.TemperatureC} degrees C and {londonWeather.Current.Condition.Description}!");
+            Console.WriteLine(string.Format(stringFormat, londonWeather.Location.Name, londonWeather.Location.Country, londonWeather.Current.TemperatureC, londonWeather.Current.Condition.Description));
 
             // Keeps the console window open at the end of the program.
             Console.ReadLine();
