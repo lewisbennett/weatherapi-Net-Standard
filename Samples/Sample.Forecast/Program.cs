6f6c045726a45ce4c734e9e9b8e58ce252875e82
Lewis <lewisbennett669@gmail.com>
2021-04-13 17:35:41 +0100

    Added forecast sample project

---
+++ Samples/Sample.Forecast/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Threading.Tasks;
+using WeatherAPI;
+
+namespace Sample.Forecast
+{
+    public static class Program
+    {
+        public static async Task Main()
+        {
+        }
+    }
+}
