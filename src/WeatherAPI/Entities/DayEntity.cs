f1d487ed251bb13cb32edad38948065b9c69c7c0
Lewis <lewisbennett669@gmail.com>
2021-04-13 16:09:21 +0100

    Began adding forecast entities

---
+++ src/WeatherAPI/Entities/DayEntity.cs
@@ -0,0 +1,98 @@
+﻿using Newtonsoft.Json;
+
+namespace WeatherAPI.Entities
+{
+    public class DayEntity
+    {
+        #region Properties
+        /// <summary>
+        /// Gets or sets the average humidity, as a percentage (0-100%).
+        /// </summary>
+        [JsonProperty("avghumidity")]
+        public int AverageHumidity { get; set; }
+
+        /// <summary>
+        /// Gets or sets the average temperature, in Celcius.
+        /// </summary>
+        [JsonProperty("avgtemp_c")]
+        public float AverageTemperatureC { get; set; }
+
+        /// <summary>
+        /// Gets or sets the average temperature, in Fahrenheit.
+        /// </summary>
+        [JsonProperty("avgtemp_f")]
+        public float AverageTemperatureF { get; set; }
+
+        /// <summary>
+        /// Gets or sets the average visibility, in kilometers.
+        /// </summary>
+        [JsonProperty("avgvis_km")]
+        public float AverageVisibilityKm { get; set; }
+
+        /// <summary>
+        /// Gets or sets the average visibility, in miles.
+        /// </summary>
+        [JsonProperty("avgvis_miles")]
+        public float AverageVisibilityMi { get; set; }
+
+        /// <summary>
+        /// Gets or sets the weather condition information.
+        /// </summary>
+        [JsonProperty("condition")]
+        public ConditionEntity Condition { get; set; }
+
+        /// <summary>
+        /// Gets or sets the maximum temperature, in Celcius.
+        /// </summary>
+        [JsonProperty("maxtemp_c")]
+        public float MaximumTemperatureC { get; set; }
+
+        /// <summary>
+        /// Gets or sets the maximum temperature, in Fahrenheit.
+        /// </summary>
+        [JsonProperty("maxtemp_f")]
+        public float MaximumTemperatureF { get; set; }
+
+        /// <summary>
+        /// Gets or sets the maximum wind speed, in kilometers per hour.
+        /// </summary>
+        [JsonProperty("maxwind_kph")]
+        public float MaximumWindKPH { get; set; }
+
+        /// <summary>
+        /// Gets or sets the maximum wind speed, in miles per hour.
+        /// </summary>
+        [JsonProperty("maxwind_mph")]
+        public float MaximumWindMPH { get; set; }
+
+        /// <summary>
+        /// Gets or sets the minimum temperature, in Celcius.
+        /// </summary>
+        [JsonProperty("mintemp_c")]
+        public float MinimumTemperatureC { get; set; }
+
+        /// <summary>
+        /// Gets or sets the minimum temperature, in Fahrenheit.
+        /// </summary>
+        [JsonProperty("mintemp_f")]
+        public float MinimumTemperatureF { get; set; }
+
+        /// <summary>
+        /// Gets or sets the total precipitation, in inches.
+        /// </summary>
+        public float TotalPrecipitationIN { get; set; }
+
+        /// <summary>
+        /// Gets or sets the total precipitation, in millimeters.
+        /// </summary>
+        [JsonProperty("totalprecip_mm")]
+        public float TotalPrecipitationMM { get; set; }
+
+        /// <summary>
+        /// Gets or sets the UV index.
+        /// </summary>
+        [JsonProperty("uv")]
+        public float UV { get; set; }
+        #endregion
+    }
+}
---
+++ src/WeatherAPI/Entities/ForecastDayEntity.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json;
+using System;
+
+namespace WeatherAPI.Entities
+{
+    public class ForecastDayEntity
+    {
+        #region Properties
+        /// <summary>
+        /// Gets or sets the date.
+        /// </summary>
+        [JsonProperty("date")]
+        public DateTime Date { get; set; }
+
+        /// <summary>
+        /// Gets or sets the forecast date, in unix time.
+        /// </summary>
+        [JsonProperty("date_epoch")]
+        public long DateEpoch { get; set; }
+
+        /// <summary>
+        /// Gets or sets the forecast information for the day.
+        /// </summary>
+        [JsonProperty("day")]
+        public DayEntity Day { get; set; }
+        #endregion
+    }
+}
