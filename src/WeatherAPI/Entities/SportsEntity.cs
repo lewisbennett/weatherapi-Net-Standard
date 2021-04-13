036490c2f23087c411c100772285e5e9835e4f45
Lewis <lewisbennett669@gmail.com>
2021-04-13 22:55:25 +0100

    Added sports entities

---
+++ src/WeatherAPI/Entities/SportsEntity.cs
@@ -0,0 +1,46 @@
+﻿using Newtonsoft.Json;
+using System;
+
+namespace WeatherAPI.Entities
+{
+    public class SportsEntity
+    {
+        #region Properties
+        /// <summary>
+        /// Gets or sets the country in which the sporting event is taking place.
+        /// </summary>
+        [JsonProperty("country")]
+        public string Country { get; set; }
+
+        /// <summary>
+        /// Gets or sets the match taking place.
+        /// </summary>
+        [JsonProperty("match")]
+        public string Match { get; set; }
+
+        /// <summary>
+        /// Gets or sets the region in which  the sporting event is taking place.
+        /// </summary>
+        [JsonProperty("region")]
+        public string Region { get; set; }
+
+        /// <summary>
+        /// Gets or sets the stadium/venue of the sporting event.
+        /// </summary>
+        [JsonProperty("stadium")]
+        public string Stadium { get; set; }
+
+        /// <summary>
+        /// Gets or sets the start of the sporting event.
+        /// </summary>
+        [JsonProperty("start")]
+        public DateTime Start { get; set; }
+
+        /// <summary>
+        /// Gets or sets the name of the tournament taking place.
+        /// </summary>
+        [JsonProperty("tournament")]
+        public string Tournament { get; set; }
+        #endregion
+    }
+}
---
+++ src/WeatherAPI/Entities/SportsResponseEntity.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json;
+
+namespace WeatherAPI.Entities
+{
+    public class SportsResponseEntity
+    {
+        #region Properties
+        /// <summary>
+        /// Gets or sets the cricket events taking place, if any.
+        /// </summary>
+        [JsonProperty("cricket")]
+        public SportsEntity[] Cricket { get; set; }
+
+        /// <summary>
+        /// Gets or sets the football events taking place, if any.
+        /// </summary>
+        [JsonProperty("football")]
+        public SportsEntity[] Football { get; set; }
+
+        /// <summary>
+        /// Gets or sets the golf events taking place, if any.
+        /// </summary>
+        [JsonProperty("golf")]
+        public SportsEntity[] Golf { get; set; }
+        #endregion
+    }
+}
