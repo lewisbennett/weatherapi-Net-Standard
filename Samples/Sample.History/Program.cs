72b638eb0d843cbbd1007406fe6af6d20bb86940
Lewis <lewisbennett669@gmail.com>
2021-04-13 23:45:39 +0100

    Added history sample

---
+++ Samples/Sample.History/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading.Tasks;
+using WeatherAPI;
+using WeatherAPI.Entities;
+
+namespace Sample.History
+{
+    public static class Program
+    {
+        public static async Task Main()
+        {
+            // Create a new WeatherAPI API client with API key.
+            var weatherApiClient = new WeatherAPIClient(Environment.GetEnvironmentVariable("API_KEY"));
+
+            var request = new HistoryRequestEntity()
+                .WithDate(DateTime.Now.AddDays(-5));
+
+            var history = await weatherApiClient.History.GetHistoryAsync(request).ConfigureAwait(false);
+
+            var stringFormat = "The forecast in {0}, {1} was:";
+
+            Console.WriteLine(string.Format(stringFormat, history.Location.Name, history.Location.Country));
+
+            foreach (var forecast in history.Forecast.ForecastDay)
+                Console.WriteLine($"{forecast.Date} - {forecast.Day.Condition.Description}");
+
+            // Keeps the console window open at the end of the program.
+            Console.ReadLine();
+        }
+    }
+}
