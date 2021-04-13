743c37ed87c1d32b8007c5197d14a465ca9d9d6d
Lewis <lewisbennett669@gmail.com>
2021-04-13 22:45:29 +0100

    Added time zone sample project

---
+++ Samples/Sample.TimeZone/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading.Tasks;
+using WeatherAPI;
+
+namespace Sample.TimeZone
+{
+    public static class Program
+    {
+        public static async Task Main()
+        {
+            // Create a new WeatherAPI API client with API key.
+            var weatherApiClient = new WeatherAPIClient(Environment.GetEnvironmentVariable("API_KEY"));
+
+            var timeZone = await weatherApiClient.TimeZones.GetTimeZoneAsync().ConfigureAwait(false);
+
+            var stringFormat = "The local time in {0}, {1} is {2}.";
+
+            Console.WriteLine(string.Format(stringFormat, timeZone.Location.Name, timeZone.Location.Country, timeZone.Location.LocalTime));
+
+            // Keeps the console window open at the end of the program.
+            Console.ReadLine();
+        }
+    }
+}
